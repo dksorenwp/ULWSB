140f6930c11a8f6544a320dc072ef62fa7345aed
dksorenwp <sorenwp@sorenwp.dk>
2023-09-16 01:17:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ else
 fi
 
 # Install packages
-packages=("nginx" "php8.2-fpm" "ffmpeg" "git")  # Replace "php-fpm" with "php8.2-fpm"
+packages=("nginx" "php-fpm" "ffmpeg" "git")  # Replace "php-fpm" with "php8.2-fpm"
 for package in "${packages[@]}"; do
     if dpkg -l | grep -q $package; then
         echo -e "\e[33mWarning: $package is already installed. If you encounter any issues, it is recommended to uninstall it and run this installation again\e[0m"
